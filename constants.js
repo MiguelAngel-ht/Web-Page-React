52fe9e64b57663e895816d92f6a8cb6db0e9da73
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-03-24 00:43:45 -0600

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,24 @@
+// links of pages
+export const HOME_PAGE_LINK = "https://www.miweb.com";
+
+// info of mangas
+const data = [
+    {
+      imgSrc: 'https://phantom-marca.unidadeditorial.es/32516217f007695d28818ff6bb89aa97/resize/1320/f/jpg/assets/multimedia/imagenes/2022/08/23/16612714132976.jpg',
+      title: 'One Piece',
+      description: 'Aventura épica de piratas en busca del tesoro legendario.'
+    },
+    {
+      imgSrc: 'https://www.gamerfocus.co/wp-content/uploads/2022/09/Hunter-x-Hunter-Gon.jpg',
+      title: 'Hunter x Hunter',
+      description: 'Un Gon persigue sus sueños en un mundo lleno de aventuras, desafíos y enemigos poderosos.'
+    },
+    {
+      imgSrc: 'https://pbs.twimg.com/media/D3GrshOWwAA03Dr.jpg:large',
+      title: 'A Quintessential Quintuplets',
+      description: 'Cinco hermanas que con ayuda de su tutor buscan sus sueños.'
+    }
+  ];
+  
+  export default data;
+  
