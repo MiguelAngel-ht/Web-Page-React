d542287f044a67e55b4f1471f98d61cf48b297bf
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-04-14 22:22:50 -0600

    Add files via upload

---
+++ components/common/index.js
@@ -0,0 +1,18 @@
+import Header from "./header";
+import Navbar from "./navbar";
+import Carousel from './carousel';
+import CardItem from './card';
+import Contact from "./contact";
+import Footer from "./footer";
+
+// import MangaPage from './mangapage';
+
+
+export {
+    Header,
+    Navbar,
+    Carousel,
+    CardItem,
+    Contact,
+    Footer
+}
