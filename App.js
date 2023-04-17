05c8c61288b834b3a1ad38234a20b38d89fea814
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-04-16 20:30:03 -0600

    Update App.js

--- App.js
+++ App.js
@@ -2,48 +2,25 @@ import React from 'react';
 import './App.css';
 
 //  IMPORTING ELEMENTS OF THE PAGE
-import {Header, Carousel, CardItem, Contact, Footer} from './components/common';
-
-// IMPORTING CONSTANT AND GLOBAL VALUES
-import data from './constants';
+import {Header, Carousel, Cards, Contact, Footer} from './components/common';
 
 // APP FUNCTION
 function App() {
   return (
     <div className="App">
+      
       <Header />
 
       <Carousel />
       
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <CardItem 
-              imgSrc={data[0].imgSrc} 
-              title={data[0].title}
-              description={data[0].description}
-            />
-          </div>
-          <div className="col-md-4">
-            <CardItem
-              imgSrc={data[1].imgSrc} 
-              title={data[1].title}
-              description={data[1].description}
-            />
-          </div>
-          <div className="col-md-4">
-            <CardItem 
-              imgSrc={data[2].imgSrc} 
-              title={data[2].title}
-              description={data[2].description} 
-            />
-          </div>
-        </div>
-      </div>
+      <center><h1>Para ver</h1></center>
+      <Cards />
 
-    <Contact />
+      <center> <h1>Contáctanos</h1> </center>
+      
+      <Contact />
 
-    <Footer />
+      <Footer />
 
     </div>
   );
