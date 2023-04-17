e7e1b6a062c77d8a18068ee647173182b195ba1d
Miguel Ángel <78463148+MiguelAngel-ht@users.noreply.github.com>
2023-04-16 20:29:14 -0600

    Add files via upload

---
+++ components/common/carditem/CardItem.jsx
@@ -0,0 +1,17 @@
+import Card from 'react-bootstrap/Card';
+import React from 'react';
+import './CardItem.css';
+
+function CardItem(props) {
+    return (
+      <Card class = "card-class" style={{ width: '18rem' }}>
+        <Card.Img class="card-image" variant="top" src={props.imgSrc}/> 
+        <Card.Body>
+          <Card.Title class='card-title'>{props.title}</Card.Title>
+          <Card.Text class='card-text'>{props.description}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
+
+export default CardItem;
